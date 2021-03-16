f839b8b02ec46b828992f2873bf9ecfbfe90a679
Pedro Igor <55034603+ORZAYAZ@users.noreply.github.com>
2021-03-16 10:47:41 -0300

    Create Palindromo.java

---
+++ Java/Palindromo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Palindromo
+{
+  static void check (String x, String s)
+  {
+    if (x.equals(s) == true)
+    {
+      System.out.println("SIM");
+    }
+    else
+    {
+      System.out.println("NAO");
+    }
+  }
+
+  static String rev (String x)
+  {
+    StringBuilder sb = new StringBuilder(x);  
+    sb.reverse();
+    return sb.toString();
+  }
+  
+  public static void main (String [] args)
+  {
+    Scanner in = new Scanner (System.in);
+    String s = in.nextLine();
+    String x = rev (s.toLowerCase());
+    check (x, s);
+    in.close();
+  }
+}
